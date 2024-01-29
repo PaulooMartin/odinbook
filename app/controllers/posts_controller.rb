e3209aad33c041152821d3099d06a3fe3b9b8d9f
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-29 15:22:25 +0800

    Filled new&create of PostController with views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,18 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new; end
+  def new
+    @post = Post.new
+  end
 
-  def create; end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def show; end
 
@@ -16,4 +25,10 @@ class PostsController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
