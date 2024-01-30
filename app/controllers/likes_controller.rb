8013f48dab9987dc098c80ab0ae5e238df8428ae
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-30 16:44:30 +0800

    Add preventive measures to LikesController

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,15 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :like_already_exists?, only: [:create]
+
   def create
     post = Post.find(params[:post])
     like = Like.build(post_id: post.id, user_id: current_user.id)
 
     if like.save
-      redirect_to root_path, notice: 'Post liked successfully'
+      redirect_to root_url, notice: 'Post liked successfully'
     else
-      redirect_to root_path, alert: 'Post like error'
+      redirect_to root_url, alert: 'Post like error'
     end
   end
 
@@ -15,6 +18,16 @@ class LikesController < ApplicationController
     like = Like.find_by(post_id: post.id, user_id: current_user.id)
     like.destroy
 
-    redirect_to root_path, notice: 'Successfully unliked a post'
+    redirect_to root_url, notice: 'Successfully unliked a post'
+  end
+
+  private
+
+  def like_already_exists?
+    post = Post.find(params[:post])
+
+    if Like.find_by(post_id: post.id, user_id: current_user.id)
+      redirect_to root_url, alert: 'You already liked this post!'
+    end
   end
 end
