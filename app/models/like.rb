6669bc9d2cf6f04d0b54e1c959049f1569123c51
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-31 14:05:53 +0800

    Fixed type usage inconsistency

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :liker, foreign_key: 'user_id', class_name: 'User', inverse_of: 'likes'
-  belongs_to :liked_post, foreign_key: 'post_id', class_name: 'Post', inverse_of: :likes
+  belongs_to :liked_post, foreign_key: 'post_id', class_name: 'Post', inverse_of: 'likes'
 
   validates_presence_of :liker, :liked_post
 end
