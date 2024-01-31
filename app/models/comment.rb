850bf5d7872ae18cbc81c53175bbd5d6462404a8
PaulooMartin <paulmartinblopez@gmail.com>
2024-01-31 14:28:14 +0800

    Add validation to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :commentor, class_name: 'User', foreign_key: 'user_id', inverse_of: 'comments'
   belongs_to :post
+
+  validates :content, presence: true, length: { maximum: 180 }
 end
