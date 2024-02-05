ee392a6659ac4d278e7c2d86f4d123b8c5edc60c
PaulooMartin <paulmartinblopez@gmail.com>
2024-02-05 14:05:02 +0800

    Add scopes to Follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,7 @@
 class Follow < ApplicationRecord
+  scope :pending, -> { where pending: true }
+  scope :accepted, -> { where pending: false }
+
   # follower refers to the person following
   # followee refers to the person being followed by the follower
   belongs_to :follower, foreign_key: 'follower_id', class_name: 'User', inverse_of: 'follow_outbound'
