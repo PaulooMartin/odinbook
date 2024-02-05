b25b0ec7a37607d49cf3a3c98bd4f075f004668e
PaulooMartin <paulmartinblopez@gmail.com>
2024-02-05 14:04:48 +0800

    Remove through association for user-follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id', inverse_of: 'commentor', dependent: :destroy
   # im unsure about the naming for :follow being follow_[direction]
   has_many :follow_outbound, class_name: 'Follow', foreign_key: 'follower_id', inverse_of: 'follower'
-  has_many :users_i_followed, through: :follow_outbound, source: 'followee'
   has_many :follow_inbound, class_name: 'Follow', foreign_key: 'followee_id', inverse_of: 'followee'
-  has_many :users_following_me, through: :follow_inbound, source: 'follower'
 
   validates_presence_of :first_name, :last_name
 
