c083a64c2059e4db9bd5768710b7dd286db2c8cc
PaulooMartin <paulmartinblopez@gmail.com>
2024-02-06 13:42:43 +0800

    Add dependent destroy to User-Follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,10 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes
   has_many :comments, foreign_key: 'user_id', inverse_of: 'commentor', dependent: :destroy
   # im unsure about the naming for :follow being follow_[direction]
-  has_many :follows_outbound, class_name: 'Follow', foreign_key: 'follower_id', inverse_of: 'follower'
-  has_many :follows_inbound, class_name: 'Follow', foreign_key: 'followee_id', inverse_of: 'followee'
+  has_many :follows_outbound, class_name: 'Follow', foreign_key: 'follower_id', inverse_of: 'follower',
+                              dependent: :destroy
+  has_many :follows_inbound, class_name: 'Follow', foreign_key: 'followee_id', inverse_of: 'followee',
+                             dependent: :destroy
 
   validates_presence_of :first_name, :last_name
 
