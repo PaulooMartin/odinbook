f5cbdd0ec126b5608d4a9d4e11e1cfb7367c74b9
PaulooMartin <paulmartinblopez@gmail.com>
2024-02-06 10:01:05 +0800

    Pluralization of association naming of User-Follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -4,6 +4,6 @@ class Follow < ApplicationRecord
 
   # follower refers to the person following
   # followee refers to the person being followed by the follower
-  belongs_to :follower, foreign_key: 'follower_id', class_name: 'User', inverse_of: 'follow_outbound'
-  belongs_to :followee, foreign_key: 'followee_id', class_name: 'User', inverse_of: 'follow_inbound'
+  belongs_to :follower, foreign_key: 'follower_id', class_name: 'User', inverse_of: 'follows_outbound'
+  belongs_to :followee, foreign_key: 'followee_id', class_name: 'User', inverse_of: 'follows_inbound'
 end
