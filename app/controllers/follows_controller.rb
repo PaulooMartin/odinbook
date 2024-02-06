6eaa57de976592f61bbccb2b29aa6c3e0b4d7750
PaulooMartin <paulmartinblopez@gmail.com>
2024-02-06 11:16:30 +0800

    Add update to FollowsController
    * this allows the user to accept follow requests

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,5 +1,6 @@
 class FollowsController < ApplicationController
   before_action :authenticate_user!
+  before_action :other_user_exists?
   before_action :follow_already_exists?, only: [:create]
   before_action :tries_follow_self?, only: [:create]
 
@@ -14,7 +15,16 @@ class FollowsController < ApplicationController
     end
   end
 
-  def update; end
+  def update
+    follower = User.find(params[:user])
+    follow = current_user.follows_inbound.pending.find_by(follower_id: follower.id)
+
+    if follow.update(pending: false)
+      redirect_to root_url, notice: "You have accepted #{follower.full_name}'s follow request"
+    else
+      redirect_to root_url, alert: "An error occured when trying to accept #{follower.full_name}'s follow request"
+    end
+  end
 
   def destroy; end
 
@@ -34,5 +44,11 @@ class FollowsController < ApplicationController
     return unless current_user == followee
 
     redirect_to root_url, alert: 'You cannot follow yourself!'
+  end
+
+  def other_user_exists?
+    other_user = User.find_by(id: params[:user])
+
+    redirect_to(root_path, alert: 'Other user does not exist!') unless other_user
   end
 end
