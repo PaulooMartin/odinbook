c9ab962762045630a748e215cd39d31d828692f9
PaulooMartin <paulmartinblopez@gmail.com>
2024-02-06 11:44:48 +0800

    Refactor FollowsController
    * init other_user to prevent the database being called every filter
    * add comments per action to indicate what role other_user plays

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,28 +1,29 @@
 class FollowsController < ApplicationController
   before_action :authenticate_user!
+  before_action :init_follow
   before_action :other_user_exists?
   before_action :follow_already_exists?, only: [:create]
   before_action :tries_follow_self?, only: [:create]
 
   def create
-    followee = User.find(params[:user])
-    follow = current_user.follows_outbound.build(followee_id: followee.id)
+    # other_user refers to person being followed
+    follow = current_user.follows_outbound.build(followee_id: @other_user.id)
 
     if follow.save
-      redirect_to root_url, notice: "Successfully followed #{followee.full_name}"
+      redirect_to root_url, notice: "Successfully followed #{@other_user.full_name}"
     else
-      redirect_to root_url, alert: "An error occurred when trying to follow #{followee.full_name}"
+      redirect_to root_url, alert: "An error occurred when trying to follow #{@other_user.full_name}"
     end
   end
 
   def update
-    follower = User.find(params[:user])
-    follow = current_user.follows_inbound.pending.find_by(follower_id: follower.id)
+    # other_user refers to follower
+    follow = current_user.follows_inbound.pending.find_by(follower_id: @other_user.id)
 
     if follow.update(pending: false)
-      redirect_to root_url, notice: "You have accepted #{follower.full_name}'s follow request"
+      redirect_to root_url, notice: "You have accepted #{@other_user.full_name}'s follow request"
     else
-      redirect_to root_url, alert: "An error occured when trying to accept #{follower.full_name}'s follow request"
+      redirect_to root_url, alert: "An error occured when trying to accept #{@other_user.full_name}'s follow request"
     end
   end
 
@@ -30,25 +31,22 @@ class FollowsController < ApplicationController
 
   private
 
-  def follow_already_exists?
-    followee = User.find(params[:user])
-
-    return unless Follow.find_by(follower_id: current_user.id, followee_id: followee.id)
-
-    redirect_to root_url, alert: "You already followed #{followee.full_name}"
-  end
-
-  def tries_follow_self?
-    followee = User.find(params[:user])
-
-    return unless current_user == followee
-
-    redirect_to root_url, alert: 'You cannot follow yourself!'
+  def init_follow
+    @other_user = User.find_by(id: params[:user])
   end
 
   def other_user_exists?
-    other_user = User.find_by(id: params[:user])
+    redirect_to(root_url, alert: 'Other user does not exist!') unless @other_user
+  end
 
-    redirect_to(root_path, alert: 'Other user does not exist!') unless other_user
+  def follow_already_exists?
+    # other_user refers to person being followed
+    follow = Follow.find_by(follower_id: current_user.id, followee_id: @other_user.id)
+    redirect_to(root_url, alert: "You already followed #{@other_user.full_name}") if follow
+  end
+
+  def tries_follow_self?
+    # other_user refers to person being followed
+    redirect_to(root_url, alert: 'You cannot follow yourself!') if current_user == @other_user
   end
 end
